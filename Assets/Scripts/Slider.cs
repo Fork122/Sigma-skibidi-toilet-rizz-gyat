fab4026240da278b77a8e6e02be281e226c9bfd2
Idan Shaviner <idan.shaviner@dit3593us.DIGIPEN.EDU>
2025-01-13 09:44:43 -0800

    scroller and checkpoint2

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -11,15 +11,22 @@ public class Slider : MonoBehaviour
     private float time;
     private float startTime;
     private bool moving = false;
+    //new
+    public GameObject player;
+    private bool onBlock = false;
+    private Rigidbody2D myRB;
     // Start is called before the first frame update
     private void Start()
     {
+        myRB = GetComponent<Rigidbody2D>();
         startPosition = transform.position;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
        if (transform.position == startPosition && !moving )
         {
             StartCoroutine(move(finalPosition));
@@ -36,15 +43,55 @@ public class Slider : MonoBehaviour
         moving = true;
         startTime = Time.time;
         Vector3 position = transform.position;
-        while (transform.position != dest)
+
+        //new
+        if (onBlock)
+        {
+            player.transform.SetParent(transform);
+        }
+
+
+        while ((dest - transform.position).sqrMagnitude > 0.1f)
         {
             yield return null;
             time = Time.time;
             // Lerp durration related
-            
-            float percentageComplete = (time - startTime) / duration;
-            transform.position = Vector3.Lerp(position, dest, percentageComplete);
+
+            //float percentageComplete = (time - startTime) / duration;
+            //transform.position = Vector3.Lerp(position, dest, percentageComplete);
+            Vector3 toddestination = dest - transform.position;
+            toddestination.Normalize();
+            myRB.velocity = new Vector2(toddestination.x * duration, toddestination.y * duration);
+        }
+        finalPosition = startPosition;
+        startPosition = transform.position;
+        //new
+       
+        if (!onBlock)
+        {
+            player.transform.SetParent(null);
         }
         moving = false;
+
     }
+    //new
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("OnCollisionEnter2D");
+        if (collision.gameObject == gameObject.CompareTag("Player"))
+        {
+            onBlock = true;
+        }
+    }
+    //new
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        print("OnCollisionExit2D");
+        if (collision.gameObject == gameObject.CompareTag("Player"))
+        {
+            onBlock = false;
+        }
+    }
+    */
 }
