1f0593e0e1ca6c99cf428e1df908a864c101f943
Fork122 <andersonmichealnathan@gmail.com>
2025-01-14 08:09:56 -0800

    dfg

--- Assets/Scripts/BetterSlider.cs
+++ Assets/Scripts/BetterSlider.cs
@@ -14,13 +14,21 @@ public class BetterSlider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position == positions[0] && !isMoving)
+        {
+            StartCoroutine(MoveF(positions));
+        }
+        else if(transform.position == positions[positions.Length -1] && !isMoving)
+        {
+            StartCoroutine(MoveB(positions));
+        }
     }
-    IEnumerator Move(Vector3[] dests)
+    IEnumerator MoveF(Vector3[] dests)
     {
+        isMoving = true;
         foreach (Vector3 dest in dests)
         {
-            isMoving = true;
+            
             time = 0;
             Vector3 position = transform.position;
             while (transform.position != dest)
@@ -30,11 +38,31 @@ public class BetterSlider : MonoBehaviour
                 float percentageCompleate = time / speed;
                 transform.position = Vector3.Lerp(position, dest, percentageCompleate);
             }
-            isMoving = false;
             
+            Pause(pauseTime);
         }
+        isMoving = false;
     }
-    void Pause(int pauseTime)
+    IEnumerator MoveB(Vector3[] dests)
+    {
+        isMoving = true;
+        for (int i = dests.Length; i >= 1; i--)
+        {
+            Vector3 dest = dests[i - 1];
+            time = 0;
+            Vector3 position = transform.position;
+            while (transform.position != dest)
+            {
+                yield return null;
+                time += Time.deltaTime;
+                float percentageCompleate = time / speed;
+                transform.position = Vector3.Lerp(position, dest, percentageCompleate);
+            }
+            Pause(pauseTime);
+        }
+        isMoving = false;
+    }
+    void Pause(float pauseTime)
     {
         float time = 0;
         while (time < pauseTime)
