fd0c46ce5c7308e52311433d1f2adbe552ce5889
Idan Shaviner <idan.shaviner@dit3593us.DIGIPEN.EDU>
2025-01-16 09:46:44 -0800

    pause menu

---
+++ Assets/Scripts/PauseMenu1.cs
@@ -0,0 +1,77 @@
+/* Name: Idan Shaviner
+ * Date: 10/15/2024
+ * Desb: make a pause menu when esc is pressed
+ */
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu1 : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public
+    // Update is called once per frame
+    //if escape is pressed pop up
+   /*
+    private void Start()
+    {
+        SceneManager.GetActiveScene();
+    }
+   */
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    //resume method
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    //pause method
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+    }
+    /*   public void LoadMenu()
+       {
+           Debug.Log("Loading Menu...");
+       }
+    */
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+/*
+    public void ResetLevel()
+    {
+        //make it go back to checkpoint 0
+        SceneManager.LoadScene(SceneManager.GetActiveScene());
+    }
+*/
+}
